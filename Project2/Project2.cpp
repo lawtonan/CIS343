546bc44b82389e0e49303c7ce0bf8709dd9560fa
lawtonan <lawtonan@mail.gvsu.edu>
2017-10-09 22:08:41 -0400

    Updated Project 2

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -3,10 +3,10 @@
 #include <algorithm>
 #include <ctime>
 
-/**
+/**************************************************************************
  * A Default constructor for the Concert Object. Sets variables to default
  * values.
- **/
+ *************************************************************************/
 Concert::Concert(){
 	this->concertName = "Default";
 	std::vector<std::string> friends;
@@ -16,7 +16,7 @@ Concert::Concert(){
 	this->date = date;
 }
 
-/**
+/**************************************************************************
  * A constructor for the Concert object that accepts all values to set each
  * Concert variable.
  *
@@ -24,21 +24,25 @@ Concert::Concert(){
  * @param friends "A vector of strings that contains a list of friends"
  * @param desire "An integer to determine the desire to attend the concert"
  * @param date "The date of the concert stored in a tm struct"
- **/
-Concert::Concert(std::string concertName, std::vector<std::string> friends, int desire, std::tm date){
+ *************************************************************************/
+Concert::Concert(std::string concertName, std::vector<std::string> friends, int desire, std::tm date) {
 	this->concertName = concertName;
 	this->friends = friends;
 	this->desire = desire;
 	this->date = date;
-  }
+}
 
-/**
+/**************************************************************************
  * An operator overloader for < that is used for the std::sort.
  *
  * @param other "A constant Consert reference to be compared to"
  * @return boolean
- **/
+ *************************************************************************/
 bool Concert::operator<(const Concert& other) const {
+	// compares each variable from tm. If they are smaller it returns
+	// true, if it is larger it returns false, otherwise move onto
+	// the next comparison. Finally checks which has the greater 
+	// desire if the date is the same
 	if (getDate().tm_year < other.getDate().tm_year)
 		return true;
 	else if (getDate().tm_year > other.getDate().tm_year)
@@ -56,82 +60,95 @@ bool Concert::operator<(const Concert& other) const {
     	else
 		return false;
 }
-
-std::ostream& operator<<(std::ostream& os, const Concert& ct)
-{
-    os << ct.getName() << " " << ct.getDate().tm_mon << "/" <<
-      ct.getDate().tm_mday << "/" << ct.getDate().tm_year;
-    return os;
+/**************************************************************************
+ * An operator overloader for << to allow printing of the Concert Object
+ *
+ * @param os "A std::ostream reference"
+ * @param ct "A constant Concert reference"
+ * @return std::ostream&
+ *************************************************************************/
+std::ostream& operator<<(std::ostream& os, const Concert& ct) {
+	// Adds the correct variable to print to osstream
+	// Formatted by NAME MONTH/DAY/YEAR
+	os << ct.getName() << " " << ct.getDate().tm_mon << "/" <<
+		ct.getDate().tm_mday << "/" << ct.getDate().tm_year;
+	return os;
 }
-/**
- * The main function of Project2. This creates 10 objects and sorts them via
- * the std::sort
+/**************************************************************************
+ * The main function of Project2. This creates 10 objects and sorts them 
+ * via the std::sort
  *
  * @return integer
- **/
+ *************************************************************************/
 int main() {
+	// A default vector used in all objects
 	std::vector<std::string> buddies;
+	
+	//Each date is hard coded and has a different date and desire.
 	std::tm date1;
 	date1.tm_year = 2017;
 	date1.tm_mon = 11;
 	date1.tm_mday = 13;
 	Concert concert1 ("Concert1", buddies, 1, date1);
-
+	
 	std::tm date2;
 	date2.tm_year = 2017;
 	date2.tm_mon = 10;
 	date2.tm_mday = 13;
 	Concert concert2 ("Concert2", buddies, 3, date2);
-
+	
 	std::tm date3;
 	date3.tm_year = 2017;
 	date3.tm_mon = 10;
 	date3.tm_mday = 12;
 	Concert concert3 ("Concert3", buddies, 2, date3);
-
+	
 	std::tm date4;
 	date4.tm_year = 2016;
 	date4.tm_mon = 5;
 	date4.tm_mday = 22;
 	Concert concert4 ("Concert4", buddies, 4, date4);
-
+	
 	std::tm date5;
 	date5.tm_year = 2016;
 	date5.tm_mon = 5;
 	date5.tm_mday = 22;
 	Concert concert5 ("Concert5", buddies, 3, date5);
-
+	
 	std::tm date6;
 	date6.tm_year = 2017;
 	date6.tm_mon = 9;
 	date6.tm_mday = 13;
 	Concert concert6 ("Concert6", buddies, 1, date6);
-
+	
 	std::tm date7;
 	date7.tm_year = 2017;
 	date7.tm_mon = 5;
 	date7.tm_mday = 13;
 	Concert concert7 ("Concert7", buddies, 3, date7);
-
+	
 	std::tm date8;
 	date8.tm_year = 2017;
 	date8.tm_mon = 5;
 	date8.tm_mday = 12;
 	Concert concert8 ("Concert8", buddies, 2, date8);
-
+	
 	std::tm date9;
 	date9.tm_year = 2017;
 	date9.tm_mon = 7;
 	date9.tm_mday = 22;
 	Concert concert9 ("Concert9", buddies, 4, date9);
-
+	
 	std::tm date10;
 	date10.tm_year = 2017;
 	date10.tm_mon = 7;
 	date10.tm_mday = 22;
 	Concert concert10 ("Concert10", buddies, 3, date10);
-
+	
+	// A vector to contain all the concert objects
 	std::vector<Concert::Concert> concertList;
+
+	// Adding all the objects to the vector
 	concertList.push_back(concert1);
 	concertList.push_back(concert2);
 	concertList.push_back(concert3);
@@ -143,17 +160,20 @@ int main() {
 	concertList.push_back(concert9);
 	concertList.push_back(concert10);
 
+	//printing the list before the sort
 	std::cout << "Before: \n";
 	for (int i = 0; i < concertList.size(); i++){
-      std::cout << concertList[i] << "\n";
+		std::cout << concertList[i] << "\n";
 		//std::cout << concertList[i].getName() << "\n";
 	}
-
+	
+	// Sortung the objects
 	std::sort(concertList.begin(), concertList.end());
 
+	// Printing the list afet the sort
 	std::cout << "\nAfter: \n";
 	for (int i = 0; i < concertList.size(); i++){
-      std::cout << concertList[i] << "\n";
+		std::cout << concertList[i] << "\n";
 		//std::cout << concertList[i].getName() << "\n";
 	}
 
