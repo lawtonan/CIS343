387f63be69fc34c1e325ebd0f479d73c3b02a0bd
lawtonan <lawtonan@mail.gvsu.edu>
2017-11-08 09:46:29 -0500

    Added classes for all weapons.

--- Project4-Hallowpocalypse/weapon.py
+++ Project4-Hallowpocalypse/weapon.py
@@ -1,3 +1,21 @@
 
 class Weapon():
-    
+    def __init__(self, name, modifier):
+        self.name = name
+        self.modifier = modifier
+
+class HershyKiss(Weapon):
+    def __init__(self):
+        super(self, "HershyKiss", 1)
+
+class SourStraw(Weapon):
+    def __init__(self):
+        pass
+
+class ChocolateBar(Weapon):
+    def __init__(self):
+        pass
+
+class NerdBomb(Weapon):
+    def __init__(self):
+        pass
