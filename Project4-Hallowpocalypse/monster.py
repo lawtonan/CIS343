714f0a2e4d0235ac687881256477bbd413b98b3b
lawtonan <lawtonan@mail.gvsu.edu>
2017-11-08 09:38:31 -0500

    Added all Monster classes

--- Project4-Hallowpocalypse/monster.py
+++ Project4-Hallowpocalypse/monster.py
@@ -1,4 +1,41 @@
 from Project4-Hallowpocalypse.observable import Observable
+from random imort randint
 
 class Monster(Observable):
-    
+    def __init__(self, health = 100, strength = 10):
+        self.health = health
+        self.strength = strength
+
+class Person(Monster):
+    def __init__(self):
+        super(self, 100, -1)
+
+class Zombie(Monster):
+    def __init__(self):
+        super(self,health(),strength())
+    def health():
+        return randint(50,100)
+    def strength():
+        return randint(0,10)
+
+class Vampire(Monster):
+    def __init__(self):
+        super(self,health(),strength())
+    def health():
+        return randint(100,200)
+    def strength():
+        return randint(10,20)
+
+class Ghouls(Monster):
+    def __init__(self):
+        super(self,health(),strength())
+    def health():
+        return randint(40,80)
+    def strength():
+        return randint(15,30)
+
+class Werewolve(Monster):
+    def __init__(self):
+        super(self,200,strength())
+    def strength():
+        return randint(0,40)
