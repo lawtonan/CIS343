add740a84d61b12c8e2070535019220d65be85a9
lawtonan <lawtonan@mail.gvsu.edu>
2017-11-20 09:44:39 -0500

    created the player.

--- Project4-Hallowpocalypse/player.py
+++ Project4-Hallowpocalypse/player.py
@@ -1,4 +1,13 @@
 from observerpattern import observer
+from random import randint
 
 class Player(observer):
-    
+    def __init__(self):
+        self.hp = randint(100,125)
+        self.attack = randint(10,20)
+        self.weapons = []
+        createWeapons()
+    def createWeapons():
+        weapons.append(HersheyKiss())
+        for i in range(9):
+            weapons.append(randWeapon())
