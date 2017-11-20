81e932f7468cdd716f36de779d38af44e063667d
lawtonan <lawtonan@mail.gvsu.edu>
2017-11-20 09:44:05 -0500

    Created Home init and generated monsters.

--- Project4-Hallowpocalypse/home.py
+++ Project4-Hallowpocalypse/home.py
@@ -1,3 +1,11 @@
 from observerpattern.observer import Observer
+from random import randint
+from random import randrange
 
 class Home(Observer):
+    def __init__(self):
+        self.monsters = []
+        createMonsters(randint(0,10))
+    def createMonsters(num):
+        for i in range(num):
+            monsters.append(randMonter())
