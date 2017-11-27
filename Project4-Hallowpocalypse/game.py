399c3340080c7ae1e6786a15b603ce41ec9c7506
lawtonan <lawtonan@mail.gvsu.edu>
2017-11-27 01:07:37 -0500

    Updated Move logic.

--- Project4-Hallowpocalypse/game.py
+++ Project4-Hallowpocalypse/game.py
@@ -31,22 +31,22 @@ class Game(Observer):
             return False
 
         if direction == "north":
-            newLoc = (self.playerLoc[0], self.playerLoc[1] + 1)
+            newLoc = (self.playerLoc[0] - 1, self.playerLoc[1])
             if self.neighborhood.inBounds(newLoc):
                 self.playerLoc = newLoc
                 return True
         elif direction == "south":
-            newLoc = (self.playerLoc[0], self.playerLoc[1] - 1)
-            if self.neighborhood.inBounds(newLoc):
-                self.playerLoc = newLoc
-                return True
-        elif direction == "east":
             newLoc = (self.playerLoc[0] + 1, self.playerLoc[1])
             if self.neighborhood.inBounds(newLoc):
                 self.playerLoc = newLoc
                 return True
+        elif direction == "east":
+            newLoc = (self.playerLoc[0], self.playerLoc[1] + 1)
+            if self.neighborhood.inBounds(newLoc):
+                self.playerLoc = newLoc
+                return True
         elif direction == "west":
-            newLoc = (self.playerLoc[0] - 1, self.playerLoc[1])
+            newLoc = (self.playerLoc[0], self.playerLoc[1] -1)
             if self.neighborhood.inBounds(newLoc):
                 self.playerLoc = newLoc
                 return True
