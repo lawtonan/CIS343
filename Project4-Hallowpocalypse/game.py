262206c36acd7d4735d1b7c7153b842dd4c2f6ba
lawtonan <lawtonan@mail.gvsu.edu>
2017-11-27 01:01:37 -0500

    Fixed Move Command.

--- Project4-Hallowpocalypse/game.py
+++ Project4-Hallowpocalypse/game.py
@@ -32,16 +32,26 @@ class Game(Observer):
 
         if direction == "north":
             newLoc = (self.playerLoc[0], self.playerLoc[1] + 1)
+            if self.neighborhood.inBounds(newLoc):
+                self.playerLoc = newLoc
+                return True
         elif direction == "south":
             newLoc = (self.playerLoc[0], self.playerLoc[1] - 1)
+            if self.neighborhood.inBounds(newLoc):
+                self.playerLoc = newLoc
+                return True
         elif direction == "east":
             newLoc = (self.playerLoc[0] + 1, self.playerLoc[1])
+            if self.neighborhood.inBounds(newLoc):
+                self.playerLoc = newLoc
+                return True
         elif direction == "west":
             newLoc = (self.playerLoc[0] - 1, self.playerLoc[1])
+            if self.neighborhood.inBounds(newLoc):
+                self.playerLoc = newLoc
+                return True
 
-        if self.neighborhood.inBounds(newLoc):
-            self.playerLoc = newLoc
-            return True
+
         return False
 
     def attackHouse(self, loc, weaponNum):
