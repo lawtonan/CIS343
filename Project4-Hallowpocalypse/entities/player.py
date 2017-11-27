875a679b112380bb21f76aff4fcb6807a542881f
lawtonan <lawtonan@mail.gvsu.edu>
2017-11-27 01:04:17 -0500

    Fixed an error with naming.

--- Project4-Hallowpocalypse/entities/player.py
+++ Project4-Hallowpocalypse/entities/player.py
@@ -13,10 +13,10 @@ class Player(Entity, Observer, Observable):
         Observable.__init__(self)
 
     def __str__(self):
-        return self._name
+        return self.name
 
     def receiveUpdate(self, info):
-        if info in self._weapons:
+        if info in self.weapons:
             print("Your uses for {} reaches 0 and falls apart!".format(info.getName()))
             self.weapons.remove(info)
     def attack(self, house, weaponNum):
@@ -26,24 +26,24 @@ class Player(Entity, Observer, Observable):
         monsters = house.getMonsterlist()
         damage = self.weapons[weaponNum].use(self.attack)
         for mon in monsters:
-            mon.takeDamage(damage, self._weapons[weaponNum])
+            mon.takeDamage(damage, self.weapons[weaponNum])
         self.weapons[weaponNum].checkStatus()
 
     def dealDamage(self, target, weaponNum):
-        if weaponNum < 0 or weaponNum >= len(self._weapons):
+        if weaponNum < 0 or weaponNum >= len(self.weapons):
             print("Weapon does not exist!")
             return
-        target.takeDamage(self._weapons[weaponNum].use(self._attack), self._weapons[weaponNum])
+        target.takeDamage(self.weapons[weaponNum].use(self.attack), self.weapons[weaponNum])
 
     def printDamage(self, amount):
         if not self.isDead():
-            print("{} takes {} damage! \t current health: {}".format(self, amount, self._health))
+            print("{} takes {} damage! \t current health: {}".format(self, amount, self.health))
         else:
-            print("{} takes {} damage! \t current health: {}".format(self, amount, self._health))
+            print("{} takes {} damage! \t current health: {}".format(self, amount, self.health))
             print("{} is defeated and transforms into a {}!".format(self, "Monster"))
 
     def printWeapons(self):
-        [print("{}: {}".format(i, weap)) for i, weap in enumerate(self._weapons)]
+        [print("{}: {}".format(i, weap)) for i, weap in enumerate(self.weapons)]
 
     def getWeapon(self, weaponNum):
-        return self._weapons[weaponNum]
+        return self.weapons[weaponNum]
